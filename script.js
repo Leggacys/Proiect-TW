f2781390989e81c72cf482511814c59397216b91
iceeye7gabi <lab1>
2021-03-27 21:48:51 +0200

    Am facut o parte din js, mai trebuie facut

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const email = document.getElementById('email');
+const password2 = document.getElementById('password2');
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  checkInputs();
+});
+
+function checkInputs(){
+  //get values from checkInputs
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+
+  if(usernameValue === ''){
+    //show error
+    //add error class
+    setErrorFor(username, "Username cannot be blank!");
+  }
+  else{
+    //add success class
+    setSuccessFor(username);
+  }
+}
+
+function setErrorFor(input, message){
+  const formControl = input.parentElement; //.form-control
+  const small = formControl.querySelector('small');
+
+  //add error message inside small
+  small.innerText = message;
+
+  //add error class
+  formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input){
+  const formControl = input.parentElement; //.form-control
+  formControl.className = 'form-control success';
+}
