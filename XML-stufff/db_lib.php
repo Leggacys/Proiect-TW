6ef83ef4890133111d6dec6f50c11f9640e0c2dc
iceeye7gabi <iceeye7@yahoo.com>
2021-06-10 00:47:15 +0300

    update 1 noaptea

---
+++ XML-stufff/db_lib.php
@@ -0,0 +1,36 @@
+<?php
+
+    $db_msg="";
+
+    function connect_db(){
+        $hostname = "localhost";
+        $username = "root";
+        $password = "";
+
+        try{
+            $db = new PDO("mysql:host=localhost;dbname=api_db", 'root', '');      
+        }catch(PDOException $e){
+            $db = -1;
+            $db_msg = "Error at connecting to DB".$e->getMessage();
+        } 
+        return $db;
+    }
+
+    function record_users($uid, $ulastname, $ufirstname, $uemail){
+            $db = connect_db();
+            if($db){
+                $sql = "INSERT INTO users (id, lastname, firstname, email) VALUES (\"$uid\", \"$ulastname\", \"$ufirstname\", \"$uemail\");";
+
+                try{
+                    echo "<p>".$sql."</p>";
+                    $r = $db->query($sql);
+                }catch(PDOException $ex){
+                    echo "Eroare la scrierea in BD:\n".$ex->getMessage();
+                }
+            }
+
+
+    }
+
+
+?>
